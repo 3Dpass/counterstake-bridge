0fccad22188ff32e5ed4404d6d949eaa560d4da3
PaulSff <ps@3dpass.org>
2025-10-15 02:03:58 -0700

    docs: readme update

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ Get the repo:
 ```bash
 git clone https://github.com/3Dpass/counterstake-bridge
 cd counterstake-bridge
+git checkout v1.1-substrate
 yarn
 ```
 
--- conf.js
+++ conf.js
@@ -196,7 +196,7 @@ exports.disableObyte = false; // Enable Obyte monitoring
 exports.min_reward_ratio = 0.005; // claim for others if the reward is at least 0.5%
 
 // Anti-spam protection for admin notifications
-exports.admin_notification_threshold = 3788; // 6947 old value // Skip notifications for old transfers (transfer_id <= this value)
+exports.admin_notification_threshold = 3668; // 6947 old value // Skip notifications for old transfers (transfer_id <= this value)
 
 // Withdrawal delay for unfinished claims check
 exports.withdraw_delay = process.env.testnet || process.env.devnet ? '1 MINUTE' : '1 HOUR'; // How long to wait before checking claims for withdrawal
